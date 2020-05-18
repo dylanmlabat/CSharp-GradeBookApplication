abfeb4e6a5cba279f63c5e0843c73b9d4fe87837
Dylan Labat <Dylan.Labat@la.gov>
2020-05-18 10:51:01 -0500

    Create GetLetterGrade override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,10 +1,27 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw InvalidOperationException();
+            }
+
+            return 'F';
+        }
+
+        private Exception InvalidOperationException()
+        {
+            throw new NotImplementedException();
         }
     }
 }
